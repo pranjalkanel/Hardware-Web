c2dda1bdd9940a7a7b98e0053f92acdfb156dd22
Pranjal Kanel <pranjaldeepkanel@gmail.com>
2021-04-24 18:28:34 +0545

    Form changes

--- HomeController.cs
+++ HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,7 +17,8 @@ namespace HardwareWeb
         }
 
         // GET: HomeController/Details/5
-        public ActionResult Details(int id)
+        [Authorize]
+        public ActionResult Details()
         {
             return View();
         }
