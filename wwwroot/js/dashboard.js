b0882f0a041163bba76524693928bdb42442d356
Pranjal Kanel <pranjaldeepkanel@gmail.com>
2021-04-28 17:27:30 +0545

    Functional Navigation

---
+++ wwwroot/js/dashboard.js
@@ -0,0 +1,13 @@
+/**
+ * SAS Custom JS (Add additional scripts here...)
+ * ------------------
+ * Author : Suman Shrestha
+ * Author URL : https://sumanshresthaa.com.np
+ */
+
+jQuery( document ).ready( function( $ ) {
+  
+  $('.js-example-basic-single').select2();
+
+
+});
